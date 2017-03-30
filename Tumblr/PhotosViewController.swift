e093a153602a68c46dbf0043e3d94d35303a5392
Nana <narayanan_eie@yahoo.com>
2017-03-29 19:56:06 -0700

    Added Network API to fetch Photos

--- Tumblr/PhotosViewController.swift
+++ Tumblr/PhotosViewController.swift
@@ -7,20 +7,62 @@
 //
 
 import UIKit
+import Foundation
 
 class PhotosViewController: UIViewController {
+
+    var posts: [NSDictionary] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
+        // Fetch remote data
+        fetchPhotoData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
+    // MARK: Fetch Photos
+
+    func fetchPhotoData() -> Void {
+
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+
+        let task : URLSessionDataTask = session.dataTask(
+            with: request as URLRequest,
+            completionHandler: { (data, response, error) in
+                if let data = data {
+                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                        with: data, options:[]) as? NSDictionary {
+                        //print("responseDictionary: \(responseDictionary)")
+
+                        // Recall there are two fields in the response dictionary, 'meta' and 'response'.
+                        // This is how we get the 'response' field
+                        let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+
+                        if let photosFetched = responseFieldDictionary["posts"] as? [NSDictionary] {
+                            self.posts = photosFetched
+                        }
+
+                        // This is where you will store the returned array of posts in your posts property
+                        // self.feeds = responseFieldDictionary["posts"] as! [NSDictionary]
+                    }
+                }
+        });
+        task.resume()
+
+    }
 
     /*
     // MARK: - Navigation
