4855791374e3fc78b93ba5613bb0ea530f0a83bf
Nana <narayanan_eie@yahoo.com>
2017-03-29 20:51:24 -0700

    Pull To Refresh Implemented

--- Tumblr/PhotosViewController.swift
+++ Tumblr/PhotosViewController.swift
@@ -20,6 +20,10 @@ class PhotosViewController: UIViewController, UITableViewDataSource {
 
         // Do any additional setup after loading the view.
 
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(control:)), for: UIControlEvents.valueChanged)
+        photosTableView.insertSubview(refreshControl, at: 0)
+
         // Fetch remote data
         fetchPhotoData()
     }
@@ -29,7 +33,7 @@ class PhotosViewController: UIViewController, UITableViewDataSource {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: Fetch Photos
+    // MARK: Network APIs
 
     func fetchPhotoData() -> Void {
 
@@ -66,6 +70,48 @@ class PhotosViewController: UIViewController, UITableViewDataSource {
         });
         task.resume()
 
+    }
+
+    func refreshControlAction(control refreshControl: UIRefreshControl) -> Void {
+
+        // Workaround to temp empty the table view
+        self.posts = []
+        photosTableView.reloadData()
+
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+
+        let task : URLSessionDataTask = session.dataTask(
+            with: request as URLRequest,
+            completionHandler: { (data, response, error) in
+                if let data = data {
+                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                        with: data, options:[]) as? NSDictionary {
+                        //print("responseDictionary: \(responseDictionary)")
+
+                        // Recall there are two fields in the response dictionary, 'meta' and 'response'.
+                        // This is how we get the 'response' field
+                        let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+
+                        if let photosFetched = responseFieldDictionary["posts"] as? [NSDictionary] {
+                            self.posts = photosFetched
+                        }
+
+                        self.photosTableView.reloadData()
+
+                        refreshControl.endRefreshing()
+
+                        // This is where you will store the returned array of posts in your posts property
+                        // self.feeds = responseFieldDictionary["posts"] as! [NSDictionary]
+                    }
+                }
+        });
+        task.resume()
     }
 
     // MARK: Table view data source
